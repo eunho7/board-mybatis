b8af5e51304b43ebf7fd96d3b9b4452934131364
eunho <76768140sa@gmail.com>
2026-01-04 23:19:46 +0900

    first commit

---
+++ src/main/java/com/example/board/dto/BoardDTO.java
@@ -0,0 +1,14 @@
+package com.example.board.dto;
+
+import lombok.Data;
+
+@Data
+public class BoardDTO {
+    private Long id;
+    private String boardWriter;
+    private String boardPass;
+    private String boardTitle;
+    private String boardContents;
+    private int boardHits;
+    private String createdAt;
+}
